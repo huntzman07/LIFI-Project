c3b9a85d85fdf32be9d7442ad6cb5fda5b21da7c
huntzman07 <yogeshyogeshwer04@gmail.com>
2026-01-06 19:41:41 +0530

    Transmitter code

---
+++ Code/Transmitter.ino
@@ -0,0 +1,23 @@
+#define LASERPIN 13
+void setup() {
+  pinMode(LASERPIN, OUTPUT);
+
+  // put your setup code here, to run once:
+
+}
+
+void loop() {//01011001
+  int bits[]= {LOW,HIGH,LOW,HIGH,HIGH,LOW,LOW,HIGH};
+  digitalWrite(LASERPIN,HIGH);
+  delay(10);
+  digitalWrite(LASERPIN,LOW);
+  for(int i=0;i<8;i++){
+    digitalWrite(LASERPIN,bits[i]);
+    delay(10);
+
+  }
+  digitalWrite(LASERPIN,LOW);
+  delay(1000);
+  // put your main code here, to run repeatedly:
+
+}
