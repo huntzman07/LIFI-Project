5c8cdac0897dc337599f57fa727c5d338143cbd5
Yogeshwer <139231048+huntzman07@users.noreply.github.com>
2026-01-06 19:43:12 +0530

    Update Receiver.ino

--- Code/Receiver.ino
+++ Code/Receiver.ino
@@ -1,13 +1,11 @@
 #define SOLARPIN A0
 #define THRESHOLD 10
 int ambientReading=0;
+
 void setup() {
   pinMode(SOLARPIN,INPUT);
   Serial.begin(9600);
   ambientReading=analogRead(SOLARPIN);
-
-  // put your setup code here, to run once:
-
 }
 
 void loop() {
@@ -22,13 +20,10 @@ void loop() {
         bits[i]=0;
       }
       delay(10);
-
     }
     for(int i=0;i<8;i++){
       Serial.print(bits[i]);
     }
     Serial.println("");
   }
-  // put your main code here, to run repeatedly:
-
 }
